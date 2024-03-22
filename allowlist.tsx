d9f5549bafb82b410505eacd97e1d16909aed07e
Hooman Dev <98424162+HoomanSafe@users.noreply.github.com>
2024-03-23 02:48:45 +0800

    Add files via upload

---
+++ allowlist.tsx
@@ -0,0 +1,24 @@
+//add your allowlists here. They need to be in the same format as you used to generate your merkle root. 
+//You can also add them here and copy the route from the console log 
+export const allowLists = new Map<string, Array<string>> ([
+    ["early",
+    [
+      "ed8a4ff24cb03557a8ffc7b8fe49a85d826c735ec83ab12ff80688a91628dff5",
+    ]],
+    ["public",
+    [
+      "1d92f7c9a2d2303a5121913757861451b53a594710f85ddb76f839ed7f08b56a",
+    ]],
+    ["nft",
+    [
+      "ed8a4ff24cb03557a8ffc7b8fe49a85d826c735ec83ab12ff80688a91628dff5",
+    ]],
+    ["token",
+    [
+      "ed8a4ff24cb03557a8ffc7b8fe49a85d826c735ec83ab12ff80688a91628dff5",
+    ]],
+    ["default",
+    [
+      "ed8a4ff24cb03557a8ffc7b8fe49a85d826c735ec83ab12ff80688a91628dff5",
+    ]],
+]);
---
+++ settings.tsx
@@ -0,0 +1,17 @@
+// Purpose: Contains texts that will be used in the UI when rendering the buttons
+// label has to be the exact same as in your candy machine config
+export const mintText = [
+  { label: "early", mintText: "Early Phase", buttonLabel: "Mint now!", header: "Early Mint" },
+  { label: "public", mintText: "Public Phase", buttonLabel: "Mint now!", header: "Public Mint" },
+  { label: "nft", mintText: "nf Phase", buttonLabel: "Mint now!", header: "nft Mint" },
+  { label: "token", mintText: "token phase", buttonLabel: "Mint now!", header: "token Mint" },
+  { label: "default", mintText: "default", buttonLabel: "Mint now!", header: "Default"}
+];
+
+//header image in the ui. replace with your own
+export const image = "https://i.ibb.co/ncdqQPr/colection.png";
+
+
+//website title
+export const headerText = "Memion Astronout ";<br></br>
+// Split the text into three lines
