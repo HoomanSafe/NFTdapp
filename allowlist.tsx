4feab690072c5f1362e46d14c880efeca49d33e0
u4ma-hdd <karhagba@gmail.com>
2024-03-28 06:03:50 -0500

    New Mint Group, Update Settings
    - Add new Mint Group
    - Fix Typo in Settings
    - Update AllowList

--- allowlist.tsx
+++ allowlist.tsx
@@ -1,6 +1,11 @@
 //add your allowlists here. They need to be in the same format as you used to generate your merkle root. 
 //You can also add them here and copy the route from the console log 
 export const allowLists = new Map<string, Array<string>> ([
+    ["wl",
+    [
+      "D3bvrTFJAANnMn3VDxapLCQUREBNPvExAktGta1L4mjt",
+      "4Uwv4UnGRuphsaJTvu8zm7DmxHFR1iFfAcxtH1x38pbD",
+    ]],
     ["early",
     [
       "ed8a4ff24cb03557a8ffc7b8fe49a85d826c735ec83ab12ff80688a91628dff5",
--- settings.tsx
+++ settings.tsx
@@ -1,10 +1,11 @@
 // Purpose: Contains texts that will be used in the UI when rendering the buttons
 // label has to be the exact same as in your candy machine config
 export const mintText = [
-  { label: "early", mintText: "Early Phase", buttonLabel: "Mint now!", header: "Early Mint" },
-  { label: "public", mintText: "Public Phase", buttonLabel: "Mint now!", header: "Public Mint" },
-  { label: "nft", mintText: "nf Phase", buttonLabel: "Mint now!", header: "nft Mint" },
-  { label: "token", mintText: "token phase", buttonLabel: "Mint now!", header: "token Mint" },
+  { label: "wl", mintText: "Connect your Whitelisted Wallet to mint NFT @ 0.025 SOL", buttonLabel: "Mint now!", header: "Whitelist Mint" },
+  { label: "early", mintText: "Grab our Early Bird rate, only on day-one. Mint NFT @ 0.05 SOL", buttonLabel: "Mint now!", header: "Early Bird Mint" },
+  { label: "nft", mintText: "Holding Memion NFT in your Wallet? Mint extra @ 0.05 SOL", buttonLabel: "Mint now!", header: "NFT Holder Mint" },
+  { label: "token", mintText: "Holding $SOLMEME Tokens in your Wallet? Mint extra @ 0.05 SOL", buttonLabel: "Mint now!", header: "Token Holder Mint" },
+  { label: "public", mintText: "Everyone else gets a chance to mint one NFT @ 0.075 SOL", buttonLabel: "Mint now!", header: "General Public Mint" },
   { label: "default", mintText: "default", buttonLabel: "Mint now!", header: "Default"}
 ];
 
@@ -13,5 +14,8 @@ export const image = "https://i.ibb.co/ncdqQPr/colection.png";
 
 
 //website title
-export const headerText = "Memion Astronout ";<br></br>
+export const headerText = "Memion Astronaut";<br></br>
 // Split the text into three lines
+
+// disable "buy me a beer feature", otherwise each mint will transfer 0.005 SOL to Original DApp DEV tip wallet
+export const NEXT_PUBLIC_BUYMARKBEER = false
